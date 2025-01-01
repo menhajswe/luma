6bebfe93d790bbc9b670d1d5be64fae4b9257297
menhajswe <menhajswe@gmail.com>
2025-01-01 18:38:16 -0500

    draft -1

--- src/test/java/basetest/BaseLumaTest.java
+++ src/test/java/basetest/BaseLumaTest.java
@@ -3,21 +3,75 @@ package basetest;
 import constants.LumaConstants;
 import org.junit.platform.commons.logging.Logger;
 import org.junit.platform.commons.logging.LoggerFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
+
 public class BaseLumaTest {
+    // update the logger to use either selenium or log4j
     protected Logger LOG = LoggerFactory.getLogger(BaseLumaTest.class);
     protected WebDriver driver;
     protected WebDriverWait wait;
+    protected int timeoutInSec = 5;
+
     protected String baseUrl = LumaConstants.BASE_URL.getValue();
 
     public BaseLumaTest(String browser) {
         driver = getWebDriverBrowser(browser);
         driver.get(baseUrl);
+    }
+
+    public void setTimeoutInSec(int timeoutInSec) {
+        this.timeoutInSec = timeoutInSec;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(timeoutInSec));
+    }
+
+    public int getTimeoutInSec() {
+        return this.timeoutInSec;
+    }
+
+    public void updateUrl(String newUrl) {
+        this.baseUrl = newUrl;
+        driver.get(newUrl);
+    }
+
+    public WebElement findElement(By locator) {
+        return driver.findElement(locator);
+    }
+
+    public void click(By locator) {
+        findElement(locator).click();
+    }
+
+    public void sendKeys(By locator, String text) {
+        findElement(locator).sendKeys(text);
+    }
+
+    public boolean isDisplayed(WebElement element) {
+        return isDisplayed(ExpectedConditions.visibilityOf(element));
+    }
+
+    public boolean isDisplayed(By locator) {
+        return isDisplayed(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    public boolean isDisplayed(ExpectedCondition expectedCondition) {
+        try {
+            wait.until(expectedCondition);
+        } catch (TimeoutException ex) {
+            LOG.error(() -> "Element not found: " + ex.getMessage() + " after waiting for " + timeoutInSec + " seconds");
+            return false;
+        }
+        return true;
     }
 
     public void setDriver(String newUrl) {
@@ -28,6 +82,8 @@ public class BaseLumaTest {
         return this.baseUrl;
     }
 
+
+    // TODO: Add capabilities to detect the host machine's OS and browser
     private WebDriver getWebDriverBrowser(String browserType) {
         return switch (browserType.toLowerCase()) {
             case "chrome" -> new ChromeDriver();
@@ -38,5 +94,11 @@ public class BaseLumaTest {
                 yield new ChromeDriver();
             }
         };
+    }
+
+    public void quit() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
--- src/test/java/pages/Cart.java
+++ src/test/java/pages/Cart.java
@@ -1,4 +1,49 @@
 package pages;
 
-public class Cart {
+import basetest.BaseLumaTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Actions;
+
+public class Cart extends BaseLumaTest {
+    // Steps:
+    // 1. visit the base url
+    // 2. click on a section of the navbar
+    // 3. hover over a sublist to open the dropdown
+    // 4. click on a link in the dropdown
+    // 5. click on a product
+    // 6. select a size
+    // 7. select a color
+    // 8. click on the add to cart button
+    // 9. click on the cart icon
+    // 10. click on the checkout button
+    // 11. fill out the checkout form
+    // 12. click on the place order button
+    // 13. verify the order was placed
+    // 14. click on the account icon
+    // 15. click on the sign-out button
+
+    String cartUrl = "";
+
+    By menDropdownLocator = By.id("ui-id-5");
+    // //a[@href='https://magento.softwaretestingboard.com/men/tops-men.html']/span[.='Tops']
+    By topsLinkLocator = By.xpath("//a[.='Tops']");
+    By menDepartmentLocator = By.xpath("//div[@id='store.menu']/nav/ul/li/a/span[.=\"Men\"]");
+
+    public Cart(String browser) {
+        super(browser);
+    }
+
+    public void clickOnMenDepartment() {
+        click(menDepartmentLocator);
+    }
+
+    public void hoverOverMenDropdown() {
+        Actions actions = new Actions(driver);
+        actions.moveToElement(findElement(menDropdownLocator)).perform();
+    }
+
+
+
+
+
 }
